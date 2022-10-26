cf705e73eba52a4fd969c937cb10f25958a3d2ff
Elvin Dsouza <emdd007@gmail.com>
2022-10-26 23:23:14 +0530

    flask pending

--- blockchain.py
+++ blockchain.py
@@ -76,4 +76,47 @@ class Blockchain:
          
         return True
     
+#creating a web app using flask
+
+app=Flask(__name__)
+
+#creating a object of the class Blockchain
+blockchain=Blockchain()
+
+#mining a new block
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block=blockchain.get_previous_block()
+    previous_proof=previous_block['proof']
+    proof=blockchain.proof_of_work(previous_proof)
+    previous_hash=blockchain.hash(previous_block)
+    block=blockchain.create_block(proof, previous_hash)
+    response={'message': 'Congratulations, you just mined a block!',
+              'index': block['index'],
+              'timestamp': block['timestamp'],
+              'proof': block['proof'],
+              'previous_hash': block['previous_hash']}
     
+    return jsonify(response), 200
+
+#displaying the full blockchain in json format
+@app.route('/get_chain', methods=['GET'])
+def display_chain():
+    response={'chain': blockchain.chain,
+              'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+#validating the blockchain
+
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    is_valid=blockchain.chain_valid(blockchain.chain)
+    if is_valid:
+        response={'message': 'All good. The Blockchain is valid.'}
+    else:
+        response={'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+    return jsonify(response), 200
+
+#running the app on flask server locally
+
+app.run(host='127.0.0.1', port=5000)
