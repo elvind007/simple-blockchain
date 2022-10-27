3449e6fa12bcf713351911d1900b596e4ec4bd64
Elvin Dsouza <emdd007@gmail.com>
2022-10-27 21:04:41 +0530

    Done

--- blockchain.py
+++ blockchain.py
@@ -1,121 +1,70 @@
-#timestamp
-
-import datetime
-
-# hash to add digital fingerprints to the blocks
-import hashlib
-
-#to store data in the blockchain
-import json
-
-#Flask is for the web app for displaying the blockchain
-from flask import Flask, jsonify
-
-class Blockchain:
-    
-    #fuction to create first block and set its hash to 0
-    def __init__(self):
-        self.chain = []
-        self.create_block(proof=1, previous_hash='0')
-    
-    #function to add more blocks to the chain    
-    def create_block(self, proof, previous_hash):
-        block={'index': len(self.chain) + 1,
-               'timestamp': str(datetime.datetime.now()),
-               'proof': proof,
-               'previous_hash': previous_hash}
-        self.chain.append(block)
-        return block
-    
-    def create_block(self, proof, previous_hash):
-        block={'index': len(self.chain) + 1,
-               'timestamp': str(datetime.datetime.now()),
-               'proof': proof,
-               'previous_hash': previous_hash}
-        self.chain.append(block)
-        return block
-    
-    #function to get the previous block
-    def get_previous_block(self):
-        return self.chain[-1]
-    
-    #function to get the proof of work
-    def proof_of_work(self, previous_proof):
-        new_proof=1
-        check_proof=False
-        while check_proof is False:
-            hash_operation=hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_operation[:4]=='0000':
-                check_proof=True
-            else:
-                new_proof += 1
-        return new_proof
-    
-    def hash(self, block):
-        encoded_block=json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(encoded_block).hexdigest()
-    
-    def chain_valid(self, chain):
-        previous_block = chain[0]
-        block_index = 1
-         
-        while block_index < len(chain):
-            block = chain[block_index]
-            if block['previous_hash'] != self.hash(previous_block):
-                return False
-               
-            previous_proof = previous_block['proof']
-            proof = block['proof']
-            hash_operation = hashlib.sha256(
-                str(proof**2 - previous_proof**2).encode()).hexdigest()
-             
-            if hash_operation[:5] != '00000':
-                return False
-            previous_block = block
-            block_index += 1
-         
-        return True
-    
-#creating a web app using flask
-
-app=Flask(__name__)
-
-#creating a object of the class Blockchain
-blockchain=Blockchain()
-
-#mining a new block
-@app.route('/mine_block', methods=['GET'])
-def mine_block():
-    previous_block=blockchain.get_previous_block()
-    previous_proof=previous_block['proof']
-    proof=blockchain.proof_of_work(previous_proof)
-    previous_hash=blockchain.hash(previous_block)
-    block=blockchain.create_block(proof, previous_hash)
-    response={'message': 'Congratulations, you just mined a block!',
-              'index': block['index'],
-              'timestamp': block['timestamp'],
-              'proof': block['proof'],
-              'previous_hash': block['previous_hash']}
-    
-    return jsonify(response), 200
-
-#displaying the full blockchain in json format
-@app.route('/get_chain', methods=['GET'])
-def display_chain():
-    response={'chain': blockchain.chain,
-              'length': len(blockchain.chain)}
-    return jsonify(response), 200
-
-#validating the blockchain
-
-@app.route('/is_valid', methods=['GET'])
-def is_valid():
-    is_valid=blockchain.chain_valid(blockchain.chain)
-    if is_valid:
-        response={'message': 'All good. The Blockchain is valid.'}
-    else:
-        response={'message': 'Houston, we have a problem. The Blockchain is not valid.'}
-    return jsonify(response), 200
-
-
-
+# importing the required libraries  
+import hashlib  
+import json  
+from time import time  
+  
+# creating the Block_chain class  
+class Block_chain(object):  
+    def __init__(self):  
+        self.chain = []  
+        self.pendingTransactions = []  
+  
+        self.newBlock(previousHash = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.", the_proof = 100)  
+  
+# Creating a new block listing key/value pairs of  
+# block information in a JSON object.  
+# Reset the list of pending transactions &  
+# append the newest block to the chain.  
+    def newBlock(self, the_proof, previousHash = None):  
+        the_block = {  
+            'index': len(self.chain) + 1,  
+            'timestamp': time(),  
+            'transactions': self.pendingTransactions,  
+            'proof': the_proof,  
+            'previous_hash': previousHash or self.hash(self.chain[-1]),  
+        }  
+        self.pendingTransactions = []  
+        self.chain.append(the_block)  
+  
+        return the_block  
+  
+#Searching the blockchain for the most recent block.  
+    @property  
+    def lastBlock(self):  
+   
+        return self.chain[-1]  
+  
+# Adding a transaction with relevant info to the 'blockpool' - list of pending tx's.   
+    def newTransaction(self, the_sender, the_recipient, the_amount):  
+        the_transaction = {  
+            'sender': the_sender,  
+            'recipient': the_recipient,  
+            'amount': the_amount  
+        }  
+        self.pendingTransactions.append(the_transaction)  
+        return self.lastBlock['index'] + 1  
+  
+# receiving one block. Turning it into a string, turning that into  
+# Unicode (for hashing). Hashing with SHA256 encryption,  
+# then translating the Unicode into a hexidecimal string.  
+    def hash(self, the_block):  
+        stringObject = json.dumps(the_block, sort_keys = True)  
+        blockString = stringObject.encode()  
+  
+        rawHash = hashlib.sha256(blockString)  
+        hexHash = rawHash.hexdigest()  
+  
+        return hexHash  
+  
+block_chain = Block_chain()  
+transaction1 = block_chain.newTransaction("Satoshi", "Alex", '10 BTC')  
+transaction2 = block_chain.newTransaction("Alex", "Satoshi", '2 BTC')  
+transaction3 = block_chain.newTransaction("Satoshi", "James", '10 BTC')  
+block_chain.newBlock(10123)  
+  
+transaction4 = block_chain.newTransaction("Alex", "Lucy", '2 BTC')  
+transaction5 = block_chain.newTransaction("Lucy", "Justin", '1 BTC')  
+transaction6 = block_chain.newTransaction("Justin", "Alex", '1 BTC')  
+block_chain.newBlock(10384)  
+  
+print("Genesis block: ", block_chain.chain)  
